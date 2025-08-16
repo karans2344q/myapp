94aa665f58ca010b87fe440e862961c93732e306
karanswarnkar <bwala9480@gmail.com>
2025-08-16 22:42:40 +0530

    firbase change

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,19 +1,23 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
+// 🔹 Naya Firebase project ka config
 const firebaseConfig = {
-  apiKey: "AIzaSyCGSRRJwQYwRsJNSgIWVlGTxskD8sGG10k",
-  authDomain: "justoneclick-6dfc7.firebaseapp.com",
-  projectId: "justoneclick-6dfc7",
-  storageBucket: "justoneclick-6dfc7.appspot.com",
-  messagingSenderId: "1052056204971",
-  appId: "1:1052056204971:web:01fe7186c6d8faff0929ee",
-  measurementId: "G-YHWPVGP3WF"
+  apiKey: "AIzaSyAENaM0DH3kQkq2F_nyNbJtHvdXgVIo87E",
+  authDomain: "justoneclick-db9e0.firebaseapp.com",
+  projectId: "justoneclick-db9e0",
+  storageBucket: "justoneclick-db9e0.firebasestorage.app",
+  messagingSenderId: "471356671018",
+  appId: "1:471356671018:web:5eabc06460e855a4b72f53",
+  measurementId: "G-P7NJD7XKC2"
 };
 
+// 🔹 Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const auth = getAuth(app); // ✅ include this
+const auth = getAuth(app);
+const analytics = getAnalytics(app); // Optional
 
 export { db, auth };
